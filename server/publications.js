55f4a8f3bf23cde3e2982b13fa503a5f2a12107f
Steve <steve@bittiger.io>
2016-01-05 16:25:04 -0500

    Added fixtures.js

--- server/publications.js
+++ server/publications.js
@@ -11,29 +11,3 @@ Meteor.publish("singleAppByAppId", function (appId) {
 });
 
 
-if(Apps.find({}).count() < 11){
-
-    var fs = Npm.require('fs');
-
-    fs.readFile('../../../../../server/data.json', 'utf8', Meteor.bindEnvironment(function(err, data) {
-            if (err) throw err;
-            var newAppData = data.split("\n");
-
-            for (var i = 0; i < newAppData.length - 1; i++) {
-                var rawAppData = JSON.parse(newAppData[i]);
-                var newApp = {};
-
-                newApp.name = rawAppData.title;
-                newApp.app_id = rawAppData.app_id;
-                newApp.developer = rawAppData.developer;
-                newApp.description = rawAppData.intro;
-                newApp.avgRating = parseInt(rawAppData.score) / 2;
-                newApp.iconUrl = rawAppData.thumbnail_url;
-                newApp.reccomendedApps = rawAppData.top_5_app;
-                Apps.insert(newApp);
-            }
-
-    }, function(err){
-        throw err;
-    }));
-}
